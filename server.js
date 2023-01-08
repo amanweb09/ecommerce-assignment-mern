d4d2e14ea0b22fa33c028a008e38e6b8bb8d88a7
Aman Khanna <amankhanna224466@gmail.com>
2023-01-08 14:27:19 +0530

    prod changes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const { PORT, COOKIE_SECRET, REACT_APP_URL } = require('./env')
 
 const express = require('express')
+const path = require('path')
 const app = express()
 const server = require('http').createServer(app)
 
@@ -15,5 +16,12 @@ require('./db/connection')()
 app.use(require('cookie-parser')(COOKIE_SECRET))
 app.use(express.json())
 app.use('/api', require('./routes'))
+
+const INDEX_PATH = path.resolve(__dirname, './client/dist', 'index.html')
+const STATIC_PATH = path.resolve(__dirname, './client/dist')
+app.use(express.static(STATIC_PATH))
+app.use('*', (req, res) => {
+    return res.status(200).sendFile(INDEX_PATH)
+})
 
 server.listen(PORT, () => console.log('Server running on port' + PORT))
